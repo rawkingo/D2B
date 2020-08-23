5aa11e5e642c18c2fd50195ea6b7c59368f9ad38
rawkingo <69311715+rawkingo@users.noreply.github.com>
2020-08-23 15:40:38 +0300

    DecToBin

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DecToBin
+{
+    class Program
+    {
+                static void Main(string[] args)
+        {
+            int  n, i;       
+       int[] a = new int[10];     
+       Console.Write("Ievadiet skaitli konvertācijai: ");    
+       n= int.Parse(Console.ReadLine());     
+       for(i=0; n>0; i++)      
+        {      
+         a[i]=n%2;      
+         n= n/2;    
+        }      
+       Console.Write("Binārais skaitlis= ");      
+       for(i=i-1 ;i>=0 ;i--)      
+       {      
+        Console.Write(a[i]);      
+       } 
+        }
+    }
+}
